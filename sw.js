8b0d4bb09d2b3165b8b83ca0aa070c18b6ad0198
uberalanoliveira-hue <uber.alanoliveira@gmail.com>
2026-03-16 17:30:21 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,2 +1,11 @@
-self.addEventListener("install", () => self.skipWaiting());
-self.addEventListener("activate", event => event.waitUntil(self.clients.claim()));
+self.addEventListener("install", (event) => {
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", (event) => {
+  // fetch handler mínimo só para atender o critério de instalabilidade
+});
